c02a8db819e139a97066ff1c698249c93e94d42c
GravesG <18870224341@163.com>
2020-02-06 22:30:04 +0800

    返回对象

---
+++ backend-utils/src/main/java/com/graves/meetingfilm/utils/common/vo/BaseResponseVO.java
@@ -0,0 +1,42 @@
+package com.graves.meetingfilm.utils.common.vo;
+
+import com.graves.meetingfilm.utils.exception.CommonServiceException;
+import lombok.Data;
+
+/**
+ * 表现层公共返回
+ */
+@Data
+public class BaseResponseVO<M> {
+
+    private Integer code;
+    private String message;
+    private M data;
+
+    private BaseResponseVO(){}
+
+    // 成功但是无参数
+    public static BaseResponseVO success(){
+        BaseResponseVO response = new BaseResponseVO();
+        response.setCode(200);
+        response.setMessage("");
+        return response;
+    }
+
+    // 成功有参数
+    public static<M> BaseResponseVO success(M data){
+        BaseResponseVO response = new BaseResponseVO();
+        response.setCode(200);
+        response.setMessage("");
+        response.setData(data);
+        return response;
+    }
+
+    // 出现业务异常
+    public static<M> BaseResponseVO serviceException(CommonServiceException e){
+        BaseResponseVO response = new BaseResponseVO();
+        response.setCode(e.getCode());
+        response.setMessage(e.getMessage());
+        return response;
+    }
+}
