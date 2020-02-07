2404145f3dca191e6ae4369b2c143e2dad9fcd20
GravesG <18870224341@163.com>
2020-02-07 16:55:44 +0800

    补全注解代码

--- backend-user/src/main/java/com/graves/meetingfilm/user/controller/UserController.java
+++ backend-user/src/main/java/com/graves/meetingfilm/user/controller/UserController.java
@@ -2,9 +2,10 @@ package com.graves.meetingfilm.user.controller;
 
 import com.graves.meetingfilm.user.controller.vo.LoginReqVO;
 import com.graves.meetingfilm.user.service.UserServiceAPI;
-import com.graves.meetingfilm.utils.common.vo.BaseRequestVO;
 import com.graves.meetingfilm.utils.common.vo.BaseResponseVO;
 import com.graves.meetingfilm.utils.exception.CommonServiceException;
+import com.graves.meetingfilm.utils.util.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +21,7 @@ import java.util.Map;
 @RequestMapping(value = "/user")
 public class UserController {
 
+    @Autowired
     private UserServiceAPI userServiceAPI;
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
@@ -30,9 +32,13 @@ public class UserController {
         //验证用户名密码是否正确
         String userId = userServiceAPI.checkUserLogin(reqVO.getUsername(), reqVO.getPassword());
 
-        Map<String,String> result = new HashMap<>();
-        result.put("randomKey","");
-        result.put("token","");
+        JwtTokenUtil tokenUtil = new JwtTokenUtil();
+        String randomKey = tokenUtil.getRandomKey();
+        String token = tokenUtil.generateToken(userId, randomKey);
+
+        Map<String, String> result = new HashMap<>();
+        result.put("randomKey", randomKey);
+        result.put("token", token);
         return BaseResponseVO.success(result);
     }
 
--- backend-user/src/main/java/com/graves/meetingfilm/user/service/UserServiceImpl.java
+++ backend-user/src/main/java/com/graves/meetingfilm/user/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.graves.meetingfilm.user.dao.entity.MoocBackendUserT;
 import com.graves.meetingfilm.user.dao.mapper.MoocBackendUserTMapper;
 import com.graves.meetingfilm.utils.exception.CommonServiceException;
 import com.graves.meetingfilm.utils.util.MD5Util;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.List;
  * @description: 用户接口实现类
  * @date 2020/2/6 23:02
  */
+@Service
 public class UserServiceImpl implements UserServiceAPI {
     @Resource
     private MoocBackendUserTMapper userMapper;
@@ -24,24 +26,24 @@ public class UserServiceImpl implements UserServiceAPI {
     public String checkUserLogin(String username, String password) throws CommonServiceException {
         // 根据用户名获取用户信息
         QueryWrapper queryWrapper = new QueryWrapper();
-        queryWrapper.eq("user_name",username);
+        queryWrapper.eq("user_name", username);
 
         // 避免数据出现问题
         List<MoocBackendUserT> list = userMapper.selectList(queryWrapper);
         MoocBackendUserT user = null;
-        if(list !=null && list.size()>0){
+        if (list != null && list.size() > 0) {
             user = list.stream().findFirst().get();
-        }else{
+        } else {
             throw new CommonServiceException(404, "用户名输入有误");
         }
 
         // 验证密码是否正确【密码要做MD5加密，才能验证是否匹配】
         String encrypt = MD5Util.encrypt(password);
 
-        if(!encrypt.equals(user.getUserPwd())){
-            throw new CommonServiceException(500,"用户密码输入有误");
-        }else{
-            return user.getUuid()+"";
+        if (!encrypt.equals(user.getUserPwd())) {
+            throw new CommonServiceException(500, "用户密码输入有误");
+        } else {
+            return user.getUuid() + "";
         }
     }
 }
