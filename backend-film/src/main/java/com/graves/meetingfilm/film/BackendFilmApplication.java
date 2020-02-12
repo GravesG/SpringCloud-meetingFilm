764bbe890bc1e9fd1951429a2e68a745cdfcccbc
GravesG <18870224341@163.com>
2020-02-12 23:16:54 +0800

    影院模块基础模块准备

---
+++ backend-film/src/main/java/com/graves/meetingfilm/film/BackendFilmApplication.java
@@ -0,0 +1,19 @@
+package com.graves.meetingfilm.film;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"com.graves.meetingfilm"})
+@MapperScan(basePackages = {"com.graves.meetingfilm.film.dao"})
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BackendFilmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendFilmApplication.class, args);
+    }
+
+}
