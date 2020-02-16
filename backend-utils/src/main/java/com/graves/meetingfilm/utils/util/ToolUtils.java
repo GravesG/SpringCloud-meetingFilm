ba6a2012dc7a02f6e3cbbe6f191c1897e560d78e
GravesG <18870224341@163.com>
2020-02-16 23:03:15 +0800

    添加字符转换方法

--- backend-utils/src/main/java/com/graves/meetingfilm/utils/util/ToolUtils.java
+++ backend-utils/src/main/java/com/graves/meetingfilm/utils/util/ToolUtils.java
@@ -1,5 +1,10 @@
 package com.graves.meetingfilm.utils.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Graves
  * @title: ToolUtils
@@ -39,5 +44,35 @@ public class ToolUtils {
         }else {
             return true;
         }
+    }
+
+    // 判断数字正则表达式
+    private static final Pattern pattern = Pattern.compile("[0-9]*");
+
+    // 检查字符串是不是int类型
+    public static boolean checkInt(String str) {
+        Matcher isNum = pattern.matcher(str);
+        if (!isNum.matches()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // 字符串转换为int类型
+    public static Integer str2Int(String str) {
+        Matcher isNum = pattern.matcher(str);
+        if (!isNum.matches()) {
+            return 0;
+        } else {
+            return Integer.parseInt(str);
+        }
+    }
+
+    // 字符串转换为LocalDateTime
+    public static LocalDateTime str2LocalDateTime(String dateStr) {
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime ldt = LocalDateTime.parse(dateStr,df);
+        return ldt;
     }
 }
