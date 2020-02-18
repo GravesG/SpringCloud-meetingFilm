61d3d665452b3461f9cc69249d2b2facf24b2308
GravesG <18870224341@163.com>
2020-02-18 23:27:32 +0800

    影厅模块基础开发

---
+++ backend-hall/src/main/java/com/graves/meetingfilm/hall/dao/mapper/MoocHallFilmInfoTMapper.java
@@ -0,0 +1,16 @@
+package com.graves.meetingfilm.hall.dao.mapper;
+
+import com.graves.meetingfilm.hall.dao.entity.MoocHallFilmInfoT;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 影厅电影信息表 Mapper 接口
+ * </p>
+ *
+ * @author graves
+ * @since 2020-02-18
+ */
+public interface MoocHallFilmInfoTMapper extends BaseMapper<MoocHallFilmInfoT> {
+
+}
