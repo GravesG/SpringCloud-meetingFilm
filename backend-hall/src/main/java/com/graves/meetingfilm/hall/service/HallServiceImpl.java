61d970538e83388a9f2c519454b16002e7038bde
GravesG <18870224341@163.com>
2020-02-23 23:45:26 +0800

    save方法

--- backend-hall/src/main/java/com/graves/meetingfilm/hall/service/HallServiceImpl.java
+++ backend-hall/src/main/java/com/graves/meetingfilm/hall/service/HallServiceImpl.java
@@ -6,7 +6,10 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.graves.meetingfilm.hall.controller.vo.HallSavedReqVO;
 import com.graves.meetingfilm.hall.controller.vo.HallsReqVO;
 import com.graves.meetingfilm.hall.controller.vo.HallsRespVO;
+import com.graves.meetingfilm.hall.dao.entity.MoocFieldT;
+import com.graves.meetingfilm.hall.dao.entity.MoocHallFilmInfoT;
 import com.graves.meetingfilm.hall.dao.mapper.MoocFieldTMapper;
+import com.graves.meetingfilm.hall.dao.mapper.MoocHallFilmInfoTMapper;
 import com.graves.meetingfilm.utils.exception.CommonServiceException;
 import com.graves.meetingfilm.utils.util.ToolUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +27,9 @@ public class HallServiceImpl implements HallServiceAPI {
 
     @Resource
     private MoocFieldTMapper fieldTMapper;
+
+    @Resource
+    private MoocHallFilmInfoTMapper hallFilmInfoTMapper;
 
     /**
     * @Author Graves
@@ -47,7 +53,27 @@ public class HallServiceImpl implements HallServiceAPI {
     }
 
     @Override
-    public void saveHall(HallSavedReqVO hallSavedReqVO) throws CommonServiceException {
+    public void saveHall(HallSavedReqVO reqVO) throws CommonServiceException {
+// 播放厅的列表数据
+        MoocFieldT field = new MoocFieldT();
 
+        field.setCinemaId(ToolUtils.str2Int(reqVO.getCinemaId()));
+        field.setFilmId(ToolUtils.str2Int(reqVO.getFilmId()));
+        field.setBeginTime(reqVO.getBeginTime());
+        field.setEndTime(reqVO.getEndTime());
+        field.setHallId(ToolUtils.str2Int(reqVO.getHallTypeId()));
+        field.setHallName(reqVO.getHallName());
+        field.setPrice(ToolUtils.str2Int(reqVO.getFilmPrice()));
+
+        fieldTMapper.insert(field);
+        // 播放厅对应的影片数据， 影片冗余数据， 缓存里有一份
+        MoocHallFilmInfoT hallFilmInfo = describeFilmInfo(reqVO.getFilmId());
+
+        hallFilmInfoTMapper.insert(hallFilmInfo);
+    }
+
+    // 播放厅对应的影片数据， 影片冗余数据， 缓存里有一份
+    private MoocHallFilmInfoT describeFilmInfo(String filmId) throws CommonServiceException{
+        return null;
     }
 }
