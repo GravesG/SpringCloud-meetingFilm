a7f6c74b5370a1b76103bd1f1bc3fb4e4a56b800
GravesG <18870224341@163.com>
2020-02-24 22:57:00 +0800

    完成模块间调用

---
+++ backend-hall/src/main/java/com/graves/meetingfilm/hall/conf/RestConf.java
@@ -0,0 +1,15 @@
+package com.graves.meetingfilm.hall.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConf {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
--- backend-hall/src/main/java/com/graves/meetingfilm/hall/service/HallServiceImpl.java
+++ backend-hall/src/main/java/com/graves/meetingfilm/hall/service/HallServiceImpl.java
@@ -1,5 +1,6 @@
 package com.graves.meetingfilm.hall.service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -12,7 +13,12 @@ import com.graves.meetingfilm.hall.dao.mapper.MoocFieldTMapper;
 import com.graves.meetingfilm.hall.dao.mapper.MoocHallFilmInfoTMapper;
 import com.graves.meetingfilm.utils.exception.CommonServiceException;
 import com.graves.meetingfilm.utils.util.ToolUtils;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 
@@ -23,6 +29,7 @@ import javax.annotation.Resource;
  * @description: TODO
  * @date 2020/2/19 23:15
  */
+@Service
 public class HallServiceImpl implements HallServiceAPI {
 
     @Resource
@@ -31,19 +38,25 @@ public class HallServiceImpl implements HallServiceAPI {
     @Resource
     private MoocHallFilmInfoTMapper hallFilmInfoTMapper;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient eurekaClient;
+
     /**
-    * @Author Graves
-    * @Description 查询影厅详情
-    * @Date  21:51
-    * @Param [hallsReqVO]
-    * @return com.baomidou.mybatisplus.core.metadata.IPage<com.graves.meetingfilm.hall.controller.vo.HallsRespVO>
-    */
+     * @return com.baomidou.mybatisplus.core.metadata.IPage<com.graves.meetingfilm.hall.controller.vo.HallsRespVO>
+     * @Author Graves
+     * @Description 查询影厅详情
+     * @Date 21:51
+     * @Param [hallsReqVO]
+     */
     @Override
     public IPage<HallsRespVO> describeHalls(HallsReqVO hallsReqVO) throws CommonServiceException {
-        Page<HallsReqVO> page = new Page<>(hallsReqVO.getNowPage(),hallsReqVO.getPageSize());
+        Page<HallsReqVO> page = new Page<>(hallsReqVO.getNowPage(), hallsReqVO.getPageSize());
 
         QueryWrapper queryWrapper = new QueryWrapper();
-        if(ToolUtils.strIsNotNull(hallsReqVO.getCinemaId())){
+        if (ToolUtils.strIsNotNull(hallsReqVO.getCinemaId())) {
             queryWrapper.eq("cinema_id", hallsReqVO.getCinemaId());
         }
 
@@ -54,7 +67,7 @@ public class HallServiceImpl implements HallServiceAPI {
 
     @Override
     public void saveHall(HallSavedReqVO reqVO) throws CommonServiceException {
-// 播放厅的列表数据
+        // 播放厅的列表数据
         MoocFieldT field = new MoocFieldT();
 
         field.setCinemaId(ToolUtils.str2Int(reqVO.getCinemaId()));
@@ -73,7 +86,33 @@ public class HallServiceImpl implements HallServiceAPI {
     }
 
     // 播放厅对应的影片数据， 影片冗余数据， 缓存里有一份
-    private MoocHallFilmInfoT describeFilmInfo(String filmId) throws CommonServiceException{
-        return null;
+    private MoocHallFilmInfoT describeFilmInfo(String filmId) throws CommonServiceException {
+        // GET REGISTER
+        ServiceInstance choose = eurekaClient.choose("film-service");
+        // 组织调用参数
+        String hostname = choose.getHost();
+        int port = choose.getPort();
+
+        String uri = "/films/" + filmId;
+
+        String url = "http://" + hostname + ":" + port + uri;
+
+        // 通过restTemplate调用影片服务
+        JSONObject baseResponseVO = restTemplate.getForObject(url, JSONObject.class);
+
+        // 解析返回值
+        JSONObject dataJson = baseResponseVO.getJSONObject("data");
+
+        // 组织参数
+        MoocHallFilmInfoT hallFilmInfo = new MoocHallFilmInfoT();
+
+        hallFilmInfo.setFilmId(dataJson.getIntValue("filmId"));
+        hallFilmInfo.setFilmName(dataJson.getString("filmName"));
+        hallFilmInfo.setFilmLength(dataJson.getString("filmLength"));
+        hallFilmInfo.setFilmCats(dataJson.getString("filmCats"));
+        hallFilmInfo.setActors(dataJson.getString("actors"));
+        hallFilmInfo.setImgAddress(dataJson.getString("imgAddress"));
+
+        return hallFilmInfo;
     }
 }
