068912e3a3150c82af2ea5c15a8f0bd9bb96f126
GravesG <18870224341@163.com>
2020-03-01 23:16:35 +0800

    queue方法

--- backend-show/backend-show-hystrix/src/test/java/com/graves/hystrix/show/command/CommandTest.java
+++ backend-show/backend-show-hystrix/src/test/java/com/graves/hystrix/show/command/CommandTest.java
@@ -3,6 +3,9 @@ package com.graves.hystrix.show.command;
 import org.checkerframework.checker.units.qual.C;
 import org.junit.Test;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 /**
  * @author Graves
  * @title: CommandTest
@@ -13,11 +16,32 @@ import org.junit.Test;
 public class CommandTest {
     @Test
     public void executeTest() {
+        long beginTime = System.currentTimeMillis();
+
         CommandDemo commandDemo = new CommandDemo("execute");
 
         //同步执行command
         String result = commandDemo.execute();
 
-        System.out.println("result=" + result);
+        long endTime = System.currentTimeMillis();
+        System.out.println("result=" + result + " , speeding=" + (endTime - beginTime));
+    }
+
+    @Test
+    public void queueTest() throws ExecutionException, InterruptedException {
+        long beginTime = System.currentTimeMillis();
+
+        CommandDemo commandDemo = new CommandDemo("queue");
+
+        Future<String> queue = commandDemo.queue();
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("future end , speeding="+(endTime-beginTime));
+
+        long endTime2 = System.currentTimeMillis();
+
+        System.out.println("result="+queue.get()+" , speeding="+(endTime2-beginTime));
+
     }
 }
