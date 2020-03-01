5721fe56c6415ba003eb4466f4c063c0e970f686
GravesG <18870224341@163.com>
2020-03-01 22:11:10 +0800

    hystrix基础模块准备

---
+++ backend-show/backend-show-hystrix/src/main/java/com/graves/meetingfilm/showhystrix/BackendShowHystrixApplication.java
@@ -0,0 +1,13 @@
+package com.graves.meetingfilm.showhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendShowHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendShowHystrixApplication.class, args);
+    }
+
+}
