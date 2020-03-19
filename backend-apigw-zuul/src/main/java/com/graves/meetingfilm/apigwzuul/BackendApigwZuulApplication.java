1a6c66d3c9e9528c6adb48b723802ff0a93d9107
GravesG <18870224341@163.com>
2020-03-19 21:43:06 +0800

    zuul基本模块构建

---
+++ backend-apigw-zuul/src/main/java/com/graves/meetingfilm/apigwzuul/BackendApigwZuulApplication.java
@@ -0,0 +1,13 @@
+package com.graves.meetingfilm.apigwzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApigwZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApigwZuulApplication.class, args);
+    }
+
+}
