32b6132ec67b342ab22fb9f51560b56f1b32dee2
GravesG <18870224341@163.com>
2020-03-22 23:55:15 +0800

    新增testNG模块

---
+++ backend-show/backend-show-testng/src/main/java/com/graves/meetingfilm/testng/BackendShowTestngApplication.java
@@ -0,0 +1,13 @@
+package com.graves.meetingfilm.testng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendShowTestngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendShowTestngApplication.class, args);
+    }
+
+}
---
+++ backend-show/backend-show-testng/src/test/java/com/graves/meetingfilm/testng/BackendShowTestngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graves.meetingfilm.testng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendShowTestngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
